89a1bfa139a9ed4b9733b2c24f26b58d96384a12
rdobrynin <roman.dobrynin@gmail.com>
2014-09-28 09:55:01 +0300

    fixed bootstrap-select

--- templates/header.php
+++ templates/header.php
@@ -11,15 +11,13 @@
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet/less" type="text/css" href="/less/style.less">
     <script src="/js/less.min.js" type="text/javascript"></script>
-<!--    bootstrap select2-->
-    <link rel="stylesheet/less" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.2/css/bootstrap-select.min.css">
-    <link rel="stylesheet/css" type="text/css" href="/assets/css/bootstrap-select.min.css">
     <!--MODERNIZER-->
     <script src="/assets/js/modernizr.min.js" type="text/javascript"></script>
     <!--bootstrap select-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="/js/main.js" type="text/javascript"></script>
     <script src="/js/retina.js" type="text/javascript"></script>
+  <script src="/assets/js/bootstrap-select.js" type="text/javascript"></script>
     <script src="/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.2/js/bootstrap-select.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
